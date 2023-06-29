6634cf0ede99308943bec2574703ce91711c90a0
jmaldonado@itpatagonia.com <jmaldonado@itpatagonia.com>
2023-06-29 09:55:23 -0300

    Deploy 1.1.0 Prod

--- api/config/defines.php
+++ api/config/defines.php
@@ -2,13 +2,13 @@
 define('VERSION', '1.1.0');
 define('VERSION_NUM', str_replace('.', '', VERSION));   //Evita conflictos en React (no acepta agregar ?v=1.8.3)
 
-define('MODE', 'Dev');
+define('MODE', 'Prod');
 define('DEBUG_MODE', false);    // Poner en true para usar las dependencias desde Dist
 
-define('PROTOCOLE_SECURE', false);
+define('PROTOCOLE_SECURE', true);
 define('PROTOCOLE', PROTOCOLE_SECURE ? 'https://': 'http://');
 
-define('HOST', PROTOCOLE . "localhost/clientes/ITP_TEMPO/api/");
+define('HOST', PROTOCOLE . "itp.gesprender.com/api/");
 define('HOST_REACT', "http://localhost:5173");
 
 if(MODE == 'Prod'){
